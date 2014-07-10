14335b5f087cb2589bc9b549b6d6cc5ee386a047
= <rebhuhnd@onid.oregonstate.edu>
2014-07-10 14:35:02 -0700

    Trying to add things for the first time.

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+    public static void main(String args[])
+    {
+        System.out.printf("Hello World!\n");
+        System.out.printf("I've been waiting for this!\n");
+    }
+}
